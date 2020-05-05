58b537163831c1660c2b6f9f5c6f6ba2c2e7d6b8
sharath <sharathnasa@gmail.com>
2020-05-05 15:45:15 +0530

    1. Added the Configuration Controller to read values from applications.properties

--- limits-service/src/main/java/com/fxpi/microservice/limitsservice/Controllers/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/fxpi/microservice/limitsservice/Controllers/LimitsConfigurationController.java
@@ -1,14 +1,19 @@
 package com.fxpi.microservice.limitsservice.Controllers;
 
 import com.fxpi.microservice.limitsservice.Bean.LimitConfiguration;
+import com.fxpi.microservice.limitsservice.Configurations.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfiguration() {
-        return new LimitConfiguration(1000, 1);
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
     }
 }
