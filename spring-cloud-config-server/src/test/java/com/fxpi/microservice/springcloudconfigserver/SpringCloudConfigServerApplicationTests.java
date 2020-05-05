6f66e571506a43c13f24700928bbe3846efa959b
sharath kumar R <fxpi.sharath@gmail.com>
2020-05-05 16:29:26 +0530

    added spring cloud config server project

---
+++ spring-cloud-config-server/src/test/java/com/fxpi/microservice/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fxpi.microservice.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
