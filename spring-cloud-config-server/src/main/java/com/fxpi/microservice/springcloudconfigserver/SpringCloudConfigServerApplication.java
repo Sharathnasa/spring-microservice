c789abb2cb0fd4ca8e95cd2646d550bb5cb2e263
sharath <sharathnasa@gmail.com>
2020-05-06 13:40:19 +0530

    1. Connected springcloud config server to local git 2. added couple of .properties file

--- spring-cloud-config-server/src/main/java/com/fxpi/microservice/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/fxpi/microservice/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -1,8 +1,13 @@
 package com.fxpi.microservice.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
