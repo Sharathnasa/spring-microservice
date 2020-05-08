1e0d636be7ccc508d7774cb4fcc7edd015e864e3
sharath <sharathnasa@gmail.com>
2020-05-08 15:36:50 +0530

    1. added hystrix support

--- limits-service/src/main/java/com/fxpi/microservice/limitsservice/Controllers/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/fxpi/microservice/limitsservice/Controllers/LimitsConfigurationController.java
@@ -2,6 +2,7 @@ package com.fxpi.microservice.limitsservice.Controllers;
 
 import com.fxpi.microservice.limitsservice.Bean.LimitConfiguration;
 import com.fxpi.microservice.limitsservice.Configurations.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,15 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfiguration() {
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration(){
+        throw new RuntimeException("Not Available");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration(){
+        return new LimitConfiguration(9, 99);
     }
 }
--- limits-service/src/main/java/com/fxpi/microservice/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/fxpi/microservice/limitsservice/LimitsServiceApplication.java
@@ -3,12 +3,14 @@ package com.fxpi.microservice.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class LimitsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LimitsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LimitsServiceApplication.class, args);
+    }
 
 }
